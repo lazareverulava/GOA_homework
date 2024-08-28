4b6759936615403a851931d518af0db43ac01fa7
lazareverulava <irakliverulava888@gmail.com>
2024-08-28 20:32:09 +0400

    commit

---
+++ level 4/homework/lesson4.py
@@ -0,0 +1,131 @@
+# მიმატება +
+print (10+12)
+print (100+29)
+print (100+10)
+print (123+123)
+print (93+12)
+print (93+88)
+print (12+12)
+print (23+32)
+print (284+1235)
+print (1334+13)
+print (123+234)
+print (383+12)
+print (123+23)
+print (123+12)
+print (144+125)
+
+#გამოკლება -
+print (10000-9999)
+print (964-64)
+print (469-98)
+print (764-99)
+print (9876-96)
+print (53-8)
+print (654-87)
+print (765-98)
+print (768-86)
+print (657-98)
+print (8776-8765)
+print (567-7)
+print (458-63)
+print (6545-876)
+print (678-87)
+
+#გამრავვლება
+print (10000*9999)
+print (964*64)
+print (469*98)
+print (764*99)
+print (9876*96)
+print (53*8)
+print (654*87)
+print (765*98)
+print (768*86)
+print (657*98)
+print (8776*8765)
+print (567*7)
+print (458*63)
+print (6545*876)
+print (678*87)
+
+#გაყოფა /
+print (10000/9999)
+print (964/64)
+print (469/98)
+print (764/99)
+print (9876/96)
+print (53/8)
+print (654/87)
+print (765/98)
+print (768/86)
+print (657/98)
+print (8776/8765)
+print (567/7)
+print (458/63)
+print (6545/876)
+print (678/87)
+
+# უნაშთო გაყოფა //
+print (10000//9999)
+print (964//64)
+print (469//98)
+print (764//99)
+print (9876//96)
+print (53//8)
+print (654//87)
+print (765//98)
+print (768//86)
+print (657//98)
+print (8776//8765)
+print (567//7)
+print (458//63)
+print (6545//876)
+print (678//87)
+
+#ხარისხში აყვანა **
+print (10000**9999)
+print (964**64)
+print (469**98)
+print (764**99)
+print (9876**96)
+print (53**8)
+print (654**87)
+print (765**98)
+print (768**86)
+print (657**98)
+print (8776**8765)
+print (567**7)
+print (458**63)
+print (6545**876)
+print (678**87)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
